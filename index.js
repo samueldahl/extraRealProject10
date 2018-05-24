4b667d3e7e16c997169f18f73cd933c6657dee8a
samuel dahl <samjoedahl@gmail.com>
2018-05-23 22:58:28 -0600

    HTML werkin, HTML dabbin the whohab.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const pug = require('pug');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
@@ -15,10 +16,11 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 
 //Environment variable wizardry below.
-MongoClient.connect(process.env.DB_URL, (err, client) => {
+MongoClient.connect(process.env.DB_URL ? process.env.DB_URL : 'mongodb://localhost', (err, client) => {
   if (err) throw err;
-  db = client.db(process.env.DB_NAME);
-  console.log(db);
+  db = client.db(process.env.DB_NAME ? process.env.DB_NAME : 'todo');
+  db.collection('todo').find();
+  // that was getting obnoxious.
 });
 
 // The below chunk of code will connect you to the base page and render the todo list.
@@ -27,6 +29,50 @@ app.get('/', async function (req, res) {
   res.render('index', {data: obj});
   console.log('Index page request fulfilled');
 });
+
+app.post('/createlist', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+app.post('/clearcompletelists', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+app.post('/clearcompletetasks', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+
+app.post('/addtask', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+app.post('/marklistcomplete', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+app.post('/deletelist', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+
+app.post('/marktaskcomplete', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+app.post('/deletetask', function (req,res){
+  console.log(req.body);
+
+  res.redirect('/');
+})
+
 
 
 
